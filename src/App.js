dcd3224f519cebf94fed545586ab6226b83eeefb
flatcalm <a_abnc@naver.com>
2023-06-22 14:25:10 +0900

    # 10. 라우터, 회원가입 컴포넌트 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import TodoTemplate from './components/TodoTemplate';
+import { Route, Routes } from 'react-router-dom';
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
+import TodoTemplate from './components/todo/TodoTemplate';
+import Login from './components/user/Login';
+import Join from './components/user/Join';
 
 function App() {
   return (
-    <TodoTemplate />
+    <>
+      <Header />
+      
+      <Routes>
+        <Route path='/' element={ <TodoTemplate /> } />
+        <Route path='/login' element={ <Login /> } />
+        <Route path='/join' element={ <Join /> } />
+      </Routes>
+
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/user/Join.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Join = () => {
+    return (
+        <div style={{ marginTop : '200px', fontSize : '48px' }}>
+            회원가입 페이지 입니다.
+        </div>
+      )
+}
+
+export default Join;
