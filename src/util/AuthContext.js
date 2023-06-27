115daa9d661b5b896516352eb2b866d589451076
flatcalm <a_abnc@naver.com>
2023-06-27 17:06:00 +0900

    # 14. 회원 권한 설정

--- src/util/AuthContext.js
+++ src/util/AuthContext.js
@@ -3,13 +3,14 @@ import React, { useEffect, useState } from "react";
 
 // 새로운 전역 Context를 생성
 const AuthContext = React.createContext({
-    isLoggedIn: false, // 로그인 했는지의 여부 추척
+    isLoggedIn: false, // 로그인 했는지의 여부 추적
     userName: '',
     onLogout : () => {}, // 더미 함수를 넣으면 자동완성 시 편함.
-    onLogin : (email, password) => {}
+    onLogin : (email, password) => {},
+    setUserInfo : () => {}
 });
 
-// 위에서 생성한 Context를 제공할 수 있는 provider
+// 위에서 생성한 Context를 제공할 수 있는 Provider
 // 이 컴포넌트를 통해 자식 컴포넌트들에게 인증 상태와 관련된 함수들을 전달할 수 있음.
 export const AuthContextProvider = props => {
 
@@ -18,7 +19,7 @@ export const AuthContextProvider = props => {
 
     // 컴포넌트가 렌더링 될 때 localStorage에서 로그인 정보를 가지고 와서 상태를 설정.
     useEffect(() => {
-        if(localStorage.getItem('isLoggedIn') === 1) {
+        if(localStorage.getItem('isLoggedIn') === '1') {
             setIsLoggedIn(true);
             setUserName(localStorage.getItem('LOGIN_USERNAME'));
         }
@@ -36,19 +37,27 @@ export const AuthContextProvider = props => {
         // json에 담긴 인증 정보를 클라이언트에 보관
         // 1. 로컬 스토리지 - 브라우저가 종료되어도 보관이 가능.
         // 2. 세션 스토리지 - 브라우저가 종료되면 사라짐.
-        localStorage.setItem('ACCESS_TOKEN', token) // key, value
-        localStorage.setItem('LOGIN_USERNAME', userName) // key, value
-        localStorage.setItem('USER_ROLE', role) // key, value
+        localStorage.setItem('ACCESS_TOKEN', token); // key, value
+        localStorage.setItem('LOGIN_USERNAME', userName); // key, value
+        localStorage.setItem('USER_ROLE', role); // key, value
         setIsLoggedIn(true);
         setUserName(userName);
     };
+
+    // 토큰 및 로그인 유저 데이터를 브라우저에 저장하는 함수
+    const setLoginUserInfo = ({ token, userName, role }) => {
+        localStorage.setItem('ACCESS_TOKEN', token);
+        localStorage.setItem('LOGIN_USERNAME', userName);
+        localStorage.setItem('USER_ROLE', role);
+    }
 
     return (
         <AuthContext.Provider value={{
             isLoggedIn,
             userName,
             onLogout : logoutHandler,
-            onLogin : loginHandler
+            onLogin : loginHandler,
+            setUserInfo : setLoginUserInfo
         }}>
             {props.children}
         </AuthContext.Provider>
